883de282cab8c39c00082714b4217757a8626cf6
Johannes Förstner <johannes@famfoerstner.de>
2021-10-24 10:22:11 +0200

    Clean up components dir

--- run.bat
+++ run.bat
@@ -27,7 +27,7 @@ robocopy "%doc_dsloa%\Bits\art" "%tmp%\Bits\art" /E /xf .gitignore /xf *.psd /xf
 robocopy "%doc_dsloa%\Bits\sound" "%tmp%\Bits\sound" /E
 :: in case where I have other resources in Bits as well, I made a subfolder
 robocopy "%doc_dsloa%\Bits\world\ai\jobs\%map%" "%tmp%\Bits\world\ai\jobs\%map%" /E
-robocopy "%doc_dsloa%\Bits\world\contentdb\components" "%tmp%\Bits\world\contentdb\components" /E
+robocopy "%doc_dsloa%\Bits\world\contentdb\components" "%tmp%\Bits\world\contentdb\components" /E /xf .gitignore /xf components.gas
 robocopy "%doc_dsloa%\Bits\world\contentdb\templates\%map%" "%tmp%\Bits\world\contentdb\templates\%map%" /E
 robocopy "%doc_dsloa%\Bits\world\global\moods\%map%" "%tmp%\Bits\world\global\moods\%map%" /E
 robocopy "%doc_dsloa%\Bits\world\global\effects" "%tmp%\Bits\world\global\effects" eos-* /S
