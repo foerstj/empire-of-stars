486116ca8a8d6cf44008b0363aa985a649616ef6
Johannes Förstner <johannes@famfoerstner.de>
2021-10-24 09:51:47 +0200

    Filter files copied from /language

--- run.bat
+++ run.bat
@@ -36,7 +36,7 @@ if %errorlevel% neq 0 pause
 
 :: Compile German language resource file
 rmdir /S /Q "%tmp%\Bits"
-robocopy "%doc_dsloa%\Bits\language" "%tmp%\Bits\language" /E
+robocopy "%doc_dsloa%\Bits\language" "%tmp%\Bits\language" eos-* /S
 %tc%\RTC.exe -source "%tmp%\Bits" -out "%ds%\DSLOA\%map_cs%-de.dsres" -copyright "CC-BY-SA 2021" -title "Empire of Stars" -author "Johannes Förstner"
 if %errorlevel% neq 0 pause
 
