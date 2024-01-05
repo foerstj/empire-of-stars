c78864852a8d68d3be4c6bb20425da1eb0e7fd0f
Johannes Förstner <johannes@famfoerstner.de>
2024-01-05 15:23:58 +0100

    Cleanup build sub-scripts

--- build-nn.bat
+++ build-nn.bat
@@ -1,12 +1,3 @@
-:: name of map, case-sensitive
-set map_cs=EoS
-:: path of DSLOA documents dir (where Bits are)
-set doc_dsloa=%USERPROFILE%\Documents\Dungeon Siege LoA
-:: path of DS installation
-set ds=.
-:: path of TankCreator
-set tc=..\TankCreator
-
 :: Compile non-nude overlay resource file
 rmdir /S /Q "%tmp%\Bits"
 robocopy "%doc_dsloa%\Bits\art" "%tmp%\Bits\art" *-nn.raw /S
@@ -18,5 +9,5 @@ for %%F in (*-nn.raw) do (
 )
 endlocal
 popd
-%tc%\RTC.exe -source "%tmp%\Bits" -out "%ds%\DSLOA\%map_cs%-nn.dsres" -copyright "CC-BY-SA 2023" -title "Empire of Stars" -author "Johannes Förstner"
+%tc%\RTC.exe -source "%tmp%\Bits" -out "%ds%\DSLOA\%map_cs%-nn.dsres" -copyright "%copyright%" -title "%title%" -author "%author%"
 if %errorlevel% neq 0 pause
